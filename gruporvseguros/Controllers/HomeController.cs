5f40b5858c1e5308a6bdd44dff23946afd587ef5
Angel <Angel@gamas>
2023-09-25 16:56:46 -0700

    primer commit

---
+++ gruporvseguros/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using gruporvseguros.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace gruporvseguros.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Contactanos()
+        {
+            return View();
+        }
+
+        public IActionResult Privacidad()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

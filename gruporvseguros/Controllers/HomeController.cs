5d74dd34c87d5d8bdaed2c387a89d256309f1da9
Angel <Angel@gamas>
2023-09-26 15:24:35 -0700

    mas productos

--- gruporvseguros/Controllers/HomeController.cs
+++ gruporvseguros/Controllers/HomeController.cs
@@ -26,6 +26,10 @@ namespace gruporvseguros.Controllers
         {
             return View();
         }
+        public IActionResult Productos()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()

3ca56e0d344441299b11aeb0880fe1fc74ff30fa
Angel <Angel@gamas>
2023-10-02 07:53:00 -0700

    prudential pagina inicio

--- gruporvseguros/Controllers/HomeController.cs
+++ gruporvseguros/Controllers/HomeController.cs
@@ -38,6 +38,10 @@ namespace gruporvseguros.Controllers
         {
             return View();
         }
+        public IActionResult Prudential()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
